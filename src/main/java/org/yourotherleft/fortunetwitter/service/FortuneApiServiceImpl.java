63b30e132538b140aa5f3ce18b5b691af44eef26
josh <joshuapaulallen@gmail.com>
2018-05-20 18:31:48 -0600

    use explicit imports

--- src/main/java/org/yourotherleft/fortunetwitter/service/FortuneApiServiceImpl.java
+++ src/main/java/org/yourotherleft/fortunetwitter/service/FortuneApiServiceImpl.java
@@ -1,7 +1,10 @@
 package org.yourotherleft.fortunetwitter.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.yourotherleft.fortunetwitter.service.type.Fortune;
